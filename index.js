02d0907de4f8ef7e7953b8b0d924fab2bbbd15d0
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-10-21 06:11:33 -0300

    mais correções

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Twitter!');
+// Servir os arquivos estáticos do build do React
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Rota para qualquer requisição que não tenha sido capturada pelos outros endpoints
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
